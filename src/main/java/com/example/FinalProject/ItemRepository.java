2a143d1dadd473507fe0533707706fb180f36583
Danish Hyder <Danish.a.hyder@gmail.com>
2022-08-01 11:32:52 -0500

    updated file structure

---
+++ src/main/java/com/example/FinalProject/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.example.FinalProject;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called itemRepository
+// CRUD refers Create, Read, Update, Delete
+public interface ItemRepository extends JpaRepository<Item, Integer>
+{
+}
